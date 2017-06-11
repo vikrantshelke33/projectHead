018928e9413eadea9289cd80bbe913720bde3b09
vikrantshelke33 <vikrant.shelke33@gmail.com>
2017-06-11 13:42:11 +0530

    Property removed

--- SpringRestful/src/com/bean/Emp.java
+++ SpringRestful/src/com/bean/Emp.java
@@ -9,15 +9,14 @@ public class Emp {
 	private String empid;
 	private String band;
 	private String buffertype;
-	private String grade;
+	
 	
 
-	public Emp(String empid, String band, String buffertype,String grade) {
+	public Emp(String empid, String band, String buffertype) {
 		super();
 		this.empid = empid;
 		this.band = band;
 		this.buffertype = buffertype;
-		this.grade = grade;
 	}
 
 	public Emp() {
@@ -45,19 +44,7 @@ public class Emp {
 		this.buffertype = buffertype;
 	}
 
-	/**
-	 * @return the grade
-	 */
-	public String getGrade() {
-		return grade;
-	}
 
-	/**
-	 * @param grade the grade to set
-	 */
-	public void setGrade(String grade) {
-		this.grade = grade;
-	}
 	
 	
 	
